72e863f2d22dcd7ee53dcbcb5015b57f20e47bd3
wltkd22 <wltkd22@naver.com>
2023-02-09 12:08:28 +0900

    회원가입 완료

---
+++ src/main/java/shop/mtcoding/myblog/model/UserRepository.java
@@ -0,0 +1,19 @@
+package shop.mtcoding.myblog.model;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserRepository {
+
+    public int insert(@Param("username") String username, @Param("password") String password,
+            @Param("email") String email);
+
+    public User findById(int id);
+
+    public int updateById(@Param("id") int id, @Param("password") String password, @Param("email") String eamil);
+
+    public int deleteById(int id);
+
+    public User findByUsername(@Param("username") String username);
+}
---
+++ src/main/java/shop/mtcoding/myblog/service/UserService.java
@@ -0,0 +1,30 @@
+package shop.mtcoding.myblog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.mtcoding.myblog.dto.user.UserReq.JoinReqDto;
+import shop.mtcoding.myblog.handler.ex.CustomException;
+import shop.mtcoding.myblog.model.User;
+import shop.mtcoding.myblog.model.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public void 회원가입(JoinReqDto joinReqDto) {
+        User sameUser = userRepository.findByUsername(joinReqDto.getUsername());
+        if (sameUser != null) {
+            throw new CustomException("동일한 username이 존재합니다");
+        }
+        int result = userRepository.insert(joinReqDto.getUsername(), joinReqDto.getPassword(), joinReqDto.getEmail());
+        if (result != 1) {
+            throw new CustomException("회원가입실패");
+        }
+    }
+
+}
