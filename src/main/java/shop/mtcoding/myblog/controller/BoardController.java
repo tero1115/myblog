8e91674d9eeb67fb9990c2f7a3d8bb216b5ad9e5
wltkd22 <wltkd22@naver.com>
2023-02-09 11:50:45 +0900

    View 연결완료

--- src/main/java/shop/mtcoding/myblog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/myblog/controller/BoardController.java
@@ -1,8 +1,29 @@
 package shop.mtcoding.myblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @GetMapping({ "/", "/board" })
+    public String main() {
+        return "board/main";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail() {
+        return "board/detail";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm() {
+
+        return "board/updateForm";
+    }
 }
