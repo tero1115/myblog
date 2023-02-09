ccb2f169f48213fc73c16694112ae496da259ec9
wltkd22 <wltkd22@naver.com>
2023-02-09 11:09:36 +0900

    initial commit

---
+++ src/test/java/shop/mtcoding/myblog/MyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
